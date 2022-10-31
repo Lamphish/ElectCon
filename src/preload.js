7d4809979b969a25971322f0fad34ca1c1576d8d
Lamphish <luiswagner1509@icloud.com>
2022-10-31 16:51:51 +0100

    created swiching patches, updated preloader

--- src/preload.js
+++ src/preload.js
@@ -1,31 +1,49 @@
 const { ipcRenderer } = require("electron");
-const menues = [
-    "menueDC",
-    "menueTS"
+let menues = [
+    "menue_DC",
+    "menue_TS"
 ]
 
+let inputs = [
+    "value1",
+    "value2",
+    "value3",
+    "value4"
+]
 //loads as soon as the site is loaded
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById(`test`).addEventListener("click", sendCon)
-    document.getElementById(`buttonTS`).addEventListener("click", menueTS)
+    console.log("DOMLoader");
+    menues.forEach(function(menue) {
+        document.getElementById(menue).style.right = "-100vw";
+    });
+    document.getElementById(("button_DC")).addEventListener("click", function() {
+        siteChange("menue_DC")
+    });
+    document.getElementById(("button_TS")).addEventListener("click", function() {
+        siteChange("menue_TS")
+    });
+    document.getElementById(("send_DC")).addEventListener("click", function() {
+        let valueArr = []
+        for(let i = 0; i <= inputs.length; i++) {
+            valueArr.join(document.getElementById(inputs[i]).value)
+        }
+        console.log(valueArr)
+        ipcRenderer.send("edit")
+    });
 });
 
-function sendCon() {
-    let input = document.getElementById(`in`).value
-    ipcRenderer.send("debug", "debug")
-    ipcRenderer.send("testCon", input)
-};
-
-function menueTS() {
-    changeMenue("menueTS")
-}
-function changeMenue(thisMenue) {
-    ipcRenderer.send("debug", "debug")
+function siteChange(thisMenue) {
     menues.forEach(function(menue) {
-        if (menue != thisMenue) {
-            document.getElementById(menue).style.display = "none"
+        if(menue == thisMenue){
+            console.log("show " + menue)
+            document.getElementById(menue).style.right = "0"
+        }else {
+            document.getElementById(menue).style.right = "-100vw"
         }
-        document.getElementById(thisMenue).style.display = "initial"
     })
 }
-    
+
+function sendCon() {
+    ipcRenderer.send("debug", "debug");
+    ipcRenderer.send("testCon", input);
+};
