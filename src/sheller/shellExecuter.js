c9e3546173da34a17757f0ced0f92efcb0e10df3
Lamphish <luiswagner1509@icloud.com>
2022-11-23 08:59:54 +0100

    Release 1.0

--- src/sheller/shellExecuter.js
+++ src/sheller/shellExecuter.js
@@ -2,7 +2,7 @@ var spawn = require("child_process").spawn,child;
 var fs = require('fs');
 
 function executer() {
-    child = spawn("powershell.exe",["../electcon-win32-x64/resources/app/powershell/dc/test.ps1"]);
+    child = spawn("powershell.exe",["../electcon-win32-x64/resources/app/powershell/dc/executable.ps1"]);
     child.stdout.on("data",function(data){
         toString(data)
         fs.appendFile('../electcon-win32-x64/resources/app/powershell/dc/log.txt', data, 'utf-8', function(err, data) {
